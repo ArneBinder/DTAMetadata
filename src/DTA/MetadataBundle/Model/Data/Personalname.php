f11ef41495bc887ce95512510ac2fff3ed58e75a
carlwitt <carlwitt@cwmacbook.fritz.box>
2013-09-27 16:09:08 +0200

    Added name formatting: 	last name, first names if the name is a simple name (containing only last and first names) and default formatting else.

--- src/DTA/MetadataBundle/Model/Data/Personalname.php
+++ src/DTA/MetadataBundle/Model/Data/Personalname.php
@@ -15,19 +15,51 @@ class Personalname extends BasePersonalname
         return $collection;
     }
     
+
+    /**
+     * Converts the name to a single string. Formats 
+     * last_name, first_name_1 first_name_2 ...
+     * if there are only first and last name fragments and 
+     * namefragment1 namefragment2 namefragment3 ...
+     * otherwise.
+     */
     public function __toString(){
 // TODO: If there should be any issue with the order, switch to the more complicated query structure.
-//        NamefragmentQuery::create()
-//                ->filterByPersonalnameId($this->getId())
-//                ->orderByRank('asc')
-//                ->find();
+        $allNF = NamefragmentQuery::create()
+                ->filterByPersonalnameId($this->getId())
+                ->joinWith('Namefragmenttype')  // no further queries needed to hydrate the namefragment types 
+                ->orderByRank('asc')            // the rank represents the order of the fragments (i.e. first name 1, last name 1, first name 2)
+                ->find();
         
-        $allNF = $this->getNamefragments();
-        $result = "";
+        $firstNames = array();
+        $lastNames  = array();
+        $simpleName = true;
         foreach($allNF as $nameFragment){
-            $result .= $nameFragment->getName() . " ";
+            $nft = $nameFragment->getNamefragmenttype()->getName();
+            switch ($nft) {
+                case "Vorname":
+                    $firstNames[] = $nameFragment->getName();
+                    break;
+                case "Nachname":
+                    $lastNames[] = $nameFragment->getName();
+                    break;
+                default: // if there are other types, the simple scheme won't fit
+                    $simpleName = false;
+                    break 2;
+            }
         }
-        return $result;
+        
+        if(count($lastNames) == 0) $simpleName = false;
+        
+        if( $simpleName ){
+            return implode(" ", $lastNames) . ", " . implode(" ", $firstNames);
+        } else {
+            $result = "";
+            foreach($allNF as $nameFragment){
+                $result .= $nameFragment->getName() . " ";
+            }
+            return $result;
+        }
     }
  
 }
