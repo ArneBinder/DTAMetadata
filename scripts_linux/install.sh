34d588b84f51c1f9d5e84f4c7a315097adc1935c
Arne Binder <arne.b.binder@gmail.coom>
2014-10-23 19:58:08 +0200

    added set permissions in linux install script

--- scripts_linux/install.sh
+++ scripts_linux/install.sh
@@ -4,9 +4,14 @@
 php composer.phar self-update
 # install needed components
 php composer.phar install
+
 # SQL Schema wird generiert
 php app/console propel:sql:build
 # PHP Code generieren
 php app/console propel:model:build
 # SQL Schema wird in die Datenbank geschrieben
 # php app/console propel:sql:insert --force
+
+# set permissions
+sudo chmod -R 077 app/cache 
+sudo chmod -R 077 app/logs 
