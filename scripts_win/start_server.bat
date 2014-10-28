2822ee24f7bdbf23ab5e191ab84a6fe24baf6a4c
Arne Binder <arne.b.binder@gmail.com>
2014-10-28 16:55:34 +0100

    views/Form/*.twig: moved SpecializedPublication to Form/ and copied the content from Publication into it. Added Entity/Book which simply extends SpecializedPublication.

--- scripts_win/start_server.bat
+++ scripts_win/start_server.bat
@@ -1,5 +1,5 @@
 @echo off
 :: %windir%\system32\runas.exe /user:postgres "pg_ctl start" | sanur.exe postgres
-start_postgres_server_sanur_pp.exe
+start_postgres_server.exe postgres postgres
 cd ..
 php app/console server:run
