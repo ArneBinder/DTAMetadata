de441a1b23450c4588533f91ffb3b943971d8a6b
Arne Binder <arne.b.binder@gmail.com>
2015-01-14 17:43:41 +0100

    added sorting by personalname (person)

--- src/DTA/MetadataBundle/Model/Data/PersonQuery.php
+++ src/DTA/MetadataBundle/Model/Data/PersonQuery.php
@@ -9,4 +9,8 @@ class PersonQuery extends BasePersonQuery implements \DTA\MetadataBundle\Model\S
     public static function sqlSort(\ModelCriteria $query, $direction = \ModelCriteria::ASC){
         return PersonalnameQuery::sqlSort($query->usePersonalnameQuery(), $direction)->endUse();
     }
+
+    public function orderByRepresentativePersonalname($direction){
+        return PersonalnameQuery::sqlSort($this->usePersonalnameQuery(), $direction)->endUse();
+    }
 }
