514897dbf0865a85c2ccb7e52922b555c9fdca8d
binder <binder@informatik.hu-berlin.de>
2015-01-21 03:34:57 +0100

    addVolume works

--- src/DTA/MetadataBundle/Model/Data/MultiVolume.php
+++ src/DTA/MetadataBundle/Model/Data/MultiVolume.php
@@ -3,6 +3,7 @@
 namespace DTA\MetadataBundle\Model\Data;
 
 use DTA\MetadataBundle\Model\Data\om\BaseMultiVolume;
+use DTA\MetadataBundle\Model\Data\om\BaseVolume;
 
 class MultiVolume extends BaseMultiVolume
 {
@@ -22,5 +23,35 @@ class MultiVolume extends BaseMultiVolume
             $result[] = $child->getSpecialization();
         }
         return $result;
+    }
+
+    /**
+     * @param Volume $newVolume
+     */
+    public function addVolume($newVolume){
+        $publication = $this->getPublication();
+        $newVolume->getPublication()
+                ->setType(PublicationPeer::TYPE_VOLUME)
+                ->setScopeValue($publication->getId())
+                ->insertAsLastChildOf($publication)
+                ->save();
+
+        $this->setVolumesTotal($this->getPublication()->countChildren())->save();
+    }
+
+    /**
+     * @param Volume $oldVolume
+     */
+    public function removeVolume($oldVolume){
+        /*$oldPublication = $oldVolume->getPublication();
+        $oldPublication->delete();
+        $oldPublication->
+        $oldVolume->getPublication()
+            ->setScopeValue($publication->getId())
+            ->insertAsLastChildOf($publication)
+            ->save();
+
+        $this->setVolumesTotal($this->getPublication()->countChildren())->save();
+        */
     }
 }
