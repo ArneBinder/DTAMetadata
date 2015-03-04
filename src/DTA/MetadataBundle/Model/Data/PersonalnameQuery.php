9d6d646d76b50d373c029af31fd9dcb2eaecc585
binder <binder@informatik.hu-berlin.de>
2015-03-04 02:15:02 +0100

    cleaned

--- src/DTA/MetadataBundle/Model/Data/PersonalnameQuery.php
+++ src/DTA/MetadataBundle/Model/Data/PersonalnameQuery.php
@@ -8,28 +8,10 @@ class PersonalnameQuery extends BasePersonalnameQuery implements \DTA\MetadataBu
 {
     public static function sqlSort(\ModelCriteria $query, $direction = \ModelCriteria::ASC) {
         /* @var $query PersonalnameQuery */
-        
         return $query->useNamefragmentQuery()
                         ->filterByType(NamefragmentPeer::TYPE_LAST_NAME)
 //                        ->orderByType(\ModelCriteria::DESC)
                         ->orderByName($direction)
                       ->endUse();
     }
-
-    /**
-     * Adds a filtering clause to the database query that filters the entities by a given string.
-     * @param $filterString the string which the entities have to contain
-     * @return the modified query object that will return the filtered entities
-     */
-
-
-    /**
-     * Adds a filtering clause to the database query that filters the entities by a given string.
-     * @param $filterString the string which the entities have to contain
-     * @return ModelCriteria the modified query object that will return the filtered entities
-     */
-    /*public static function sqlFilter(\ModelCriteria $query, $filterString)
-    {
-        return NamefragmentQuery::sqlFilter($query->useNamefragmentQuery(),$filterString)->endUse();
-    }*/
 }
