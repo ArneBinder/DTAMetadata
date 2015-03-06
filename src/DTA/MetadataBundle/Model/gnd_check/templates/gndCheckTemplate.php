d2e93608e343543c075c508b8f3e7c4e04424b5a
binder <binder@informatik.hu-berlin.de>
2015-03-06 22:57:52 +0100

    fixed personControls: if curl doesn't work, just display the gnd

---
+++ src/DTA/MetadataBundle/Model/gnd_check/templates/gndCheckTemplate.php
@@ -0,0 +1,21 @@
+
+public function gndExists(){
+    $gnd = $this->getGnd();
+    if($gnd === null or $gnd === "")
+        return null;
+    try{
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://d-nb.info/gnd/$gnd/about/html");
+        curl_setopt($ch, CURLOPT_HEADER, TRUE);
+        curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+        curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return $httpCode;
+    }catch(\Exception $e){
+        return null;
+    }
+}
