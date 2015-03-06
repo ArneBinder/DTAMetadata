eaea58930225c2688e99549e63987f6055f484d6
binder <binder@informatik.hu-berlin.de>
2015-03-06 23:26:57 +0100

    fixed personControls (if curl is disabled, the link is displayed anyway)

--- src/DTA/MetadataBundle/Model/gnd_check/templates/gndCheckTemplate.php
+++ src/DTA/MetadataBundle/Model/gnd_check/templates/gndCheckTemplate.php
@@ -3,11 +3,10 @@ public function gndExists(){
     $gnd = $this->getGnd();
     if($gnd === null or $gnd === "")
         return null;
-    try{
-        $ch = curl_init();
-    }catch(\Exception $e){
-        return $e->getMessage();
+    if(!function_exists('curl_init')){
+        return null;
     }
+    $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "http://d-nb.info/gnd/$gnd/about/html");
     curl_setopt($ch, CURLOPT_HEADER, TRUE);
     curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body
